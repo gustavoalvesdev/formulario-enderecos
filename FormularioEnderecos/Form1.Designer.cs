83fc062482b244d30192a1efac75f9aaa1bda90a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-09-15 18:44:12 -0300

    pasta com o codigo fonte do projeto

---
+++ FormularioEnderecos/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace ApiViaCep2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mTxtCep = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLocalidade = new System.Windows.Forms.TextBox();
+            this.txtIbge = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUnidade = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CEP:";
+            // 
+            // mTxtCep
+            // 
+            this.mTxtCep.Location = new System.Drawing.Point(30, 43);
+            this.mTxtCep.Mask = "00000-999";
+            this.mTxtCep.Name = "mTxtCep";
+            this.mTxtCep.Size = new System.Drawing.Size(70, 20);
+            this.mTxtCep.TabIndex = 1;
+            this.mTxtCep.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtUf);
+            this.groupBox1.Controls.Add(this.txtGia);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtLocalidade);
+            this.groupBox1.Controls.Add(this.txtIbge);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtUnidade);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtBairro);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtComplemento);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtLogradouro);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCep);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(30, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 141);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações do Endereço:";
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(515, 98);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(152, 20);
+            this.txtGia.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(512, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "GIA:";
+            // 
+            // txtLocalidade
+            // 
+            this.txtLocalidade.Location = new System.Drawing.Point(22, 99);
+            this.txtLocalidade.Name = "txtLocalidade";
+            this.txtLocalidade.Size = new System.Drawing.Size(174, 20);
+            this.txtLocalidade.TabIndex = 13;
+            // 
+            // txtIbge
+            // 
+            this.txtIbge.Location = new System.Drawing.Point(403, 98);
+            this.txtIbge.Name = "txtIbge";
+            this.txtIbge.Size = new System.Drawing.Size(106, 20);
+            this.txtIbge.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Localidade:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(400, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "IBGE:";
+            // 
+            // txtUnidade
+            // 
+            this.txtUnidade.Location = new System.Drawing.Point(268, 98);
+            this.txtUnidade.Name = "txtUnidade";
+            this.txtUnidade.Size = new System.Drawing.Size(129, 20);
+            this.txtUnidade.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(265, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Unidade:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(515, 48);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(152, 20);
+            this.txtBairro.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(512, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Bairro:";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(313, 48);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(196, 20);
+            this.txtComplemento.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Complemento:";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(106, 48);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.Size = new System.Drawing.Size(201, 20);
+            this.txtLogradouro.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Logradouro:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CEP:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(106, 41);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(621, 229);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(22, 48);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(78, 20);
+            this.txtCep.TabIndex = 1;
+            // 
+            // txtUf
+            // 
+            this.txtUf.Location = new System.Drawing.Point(202, 98);
+            this.txtUf.Name = "txtUf";
+            this.txtUf.Size = new System.Drawing.Size(60, 20);
+            this.txtUf.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(199, 82);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(24, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "UF:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 278);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.mTxtCep);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Endereços";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mTxtCep;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLocalidade;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIbge;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUnidade;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.TextBox txtCep;
+    }
+}
+
---
+++ FormularioEnderecos/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+
+namespace ApiViaCep2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (mTxtCep.TextLength == 9)
+            {
+                try
+                {
+                    RestClient restClient = new RestClient(string.Format("https://viacep.com.br/ws/{0}/json/", mTxtCep.Text));
+                    RestRequest restRequest = new RestRequest(Method.GET);
+
+                    IRestResponse restResponse = restClient.Execute(restRequest);
+
+                    if (restResponse.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        MessageBox.Show("Houve um problema com sua requisição: " + restResponse.Content, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else
+                    {
+                        DadosRetorno dadosRetorno = new JsonDeserializer().Deserialize<DadosRetorno>(restResponse);
+
+                        txtCep.Text = dadosRetorno.Cep;
+                        txtLogradouro.Text = dadosRetorno.Logradouro;
+                        txtComplemento.Text = dadosRetorno.Complemento;
+                        txtBairro.Text = dadosRetorno.Bairro;
+                        txtLocalidade.Text = dadosRetorno.Localidade;
+                        txtUf.Text = dadosRetorno.Uf;
+                        txtUnidade.Text = dadosRetorno.Unidade;
+                        txtIbge.Text = dadosRetorno.Ibge;
+                        txtGia.Text = dadosRetorno.Gia;
+                    }
+                }
+                catch(Exception erro)
+                {
+                    MessageBox.Show("Erro Geral ao Consultar a API: " + erro.Message, "Erro Geral!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair da aplicação?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+    class DadosRetorno
+    {
+        public String Cep { get; set; }
+        public String Logradouro { get; set; }
+        public String Complemento { get; set; }
+        public String Bairro { get; set; }
+        public String Localidade { get; set; }
+        public String Uf { get; set; }
+        public String Unidade { get; set; }
+        public String Ibge { get; set; }
+        public String Gia { get; set; }
+    }
+}
